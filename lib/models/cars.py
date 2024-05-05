1f9158a68c402dea99bc7fc30ae41447c2dd4bfb
btsalwa <brian.tsalwa@student.moringaschool.com>
2024-05-05 20:52:49 +0300

    cars model, carstore class

---
+++ lib/models/cars.py
@@ -0,0 +1,8 @@
+
+rent_per_hour = int(9)
+rent_per_day = int(22)
+rent_per_week = int(100)
+
+class carStore:
+    def __init__(self , number_of_cars = 0):
+        self.available_cars = number_of_cars
